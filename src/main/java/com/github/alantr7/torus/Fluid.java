6fae07fdbf7afa01afffaae7f8b13fb6aa8c5794
alantr7 <alan.trumic066@gmail.com>
2025-09-19 22:46:33 +0200

    added Fluid and FluidContainer

---
+++ src/main/java/com/github/alantr7/torus/Fluid.java
@@ -0,0 +1,7 @@
+package com.github.alantr7.torus;
+
+public enum Fluid {
+
+    WATER, LAVA,
+
+}
---
+++ src/main/java/com/github/alantr7/torus/structure/FluidContainer.java
@@ -0,0 +1,35 @@
+package com.github.alantr7.torus.structure;
+
+import com.github.alantr7.torus.Fluid;
+import org.jetbrains.annotations.Nullable;
+
+public interface FluidContainer {
+
+    @Nullable
+    Fluid getFluid();
+
+    int getFluidCapacity();
+
+    int getStoredFluid();
+
+    void setStoredFluid(int fluid);
+
+    default boolean hasSufficientFluid(int amount) {
+        return getStoredFluid() >= amount;
+    }
+
+    default int consumeFluid(int amount) {
+        int starting = getStoredFluid();
+        setStoredFluid(Math.max(starting - amount, 0));
+
+        return starting - getStoredFluid();
+    }
+
+    default int supplyFluid(int amount) {
+        int starting = getStoredFluid();
+        setStoredFluid(Math.min(starting + amount, getFluidCapacity()));
+
+        return getStoredFluid() - starting;
+    }
+
+}
