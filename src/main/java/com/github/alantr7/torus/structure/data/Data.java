7fc7166d879374d66ea89ad436141ea78fb84d30
alantr7 <alan.trumic066@gmail.com>
2025-10-05 21:10:18 +0200

    changed Byte[] to byte[]

--- src/main/java/com/github/alantr7/torus/structure/data/Data.java
+++ src/main/java/com/github/alantr7/torus/structure/data/Data.java
@@ -31,7 +31,7 @@ public class Data<T> {
         public static final Type<Integer>   INT = new Type<>(1, "int");
         public static final Type<Float>     FLOAT = new Type<>(2, "float");
         public static final Type<String>    STRING = new Type<>(3, "string");
-        public static final Type<Byte[]>    BYTE_ARRAY = new Type<>(4, "byte_array");
+        public static final Type<byte[]>    BYTE_ARRAY = new Type<>(4, "byte_array");
 
         public final int id;
 
