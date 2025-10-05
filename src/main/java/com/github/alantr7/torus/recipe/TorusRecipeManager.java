615093612dcdbc82ca6b4b215a4b53f0e6cc3662
alantr7 <alan.trumic066@gmail.com>
2025-10-05 20:50:52 +0200

    added more crusher/washer/smelting recipes

--- src/main/java/com/github/alantr7/torus/recipe/TorusRecipeManager.java
+++ src/main/java/com/github/alantr7/torus/recipe/TorusRecipeManager.java
@@ -6,8 +6,12 @@ import com.github.alantr7.torus.TorusPlugin;
 import com.github.alantr7.torus.item.TorusItem;
 import com.github.alantr7.torus.machine.OreCrusher;
 import com.github.alantr7.torus.machine.OreWasher;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.FurnaceRecipe;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.RecipeChoice;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -81,11 +85,18 @@ public class TorusRecipeManager {
 
     @Invoke(Invoke.Schedule.AFTER_PLUGIN_ENABLE)
     private void registerDefaultRecipes() {
-        registerCrusherRecipe(new CrusherRecipe("torus:crushed_iron", new RecipeIngredient(Material.IRON_ORE), new ItemStack(Material.RAW_IRON, 2), 4));
-        registerCrusherRecipe(new CrusherRecipe("torus:crushed_gold", new RecipeIngredient(Material.GOLD_ORE), new ItemStack(Material.RAW_GOLD, 2), 8));
+        registerCrusherRecipe(new CrusherRecipe("torus:crushed_coal", new RecipeIngredient(Material.COAL_ORE), new ItemStack(Material.COAL, 5), 2));
+        registerCrusherRecipe(new CrusherRecipe("torus:crushed_copper", new RecipeIngredient(Material.COPPER_ORE), new ItemStack(Material.RAW_COPPER, 2), 5));
+        registerCrusherRecipe(new CrusherRecipe("torus:crushed_iron", new RecipeIngredient(Material.IRON_ORE), new ItemStack(Material.RAW_IRON, 2), 5));
+        registerCrusherRecipe(new CrusherRecipe("torus:crushed_gold", new RecipeIngredient(Material.GOLD_ORE), new ItemStack(Material.RAW_GOLD, 2), 7));
 
-        registerWasherRecipe(new WasherRecipe("torus:iron_dust", new RecipeIngredient(Material.RAW_IRON), TorusItem.getById("torus:iron_dust").toItemStack(), 5));
+        registerWasherRecipe(new WasherRecipe("torus:iron_dust", new RecipeIngredient(Material.RAW_IRON), TorusItem.getById("torus:iron_dust").toItemStack(), 8));
+        registerWasherRecipe(new WasherRecipe("torus:copper_dust", new RecipeIngredient(Material.RAW_COPPER), TorusItem.getById("torus:copper_dust").toItemStack(), 8));
         registerWasherRecipe(new WasherRecipe("torus:gold_dust", new RecipeIngredient(Material.RAW_GOLD), TorusItem.getById("torus:gold_dust").toItemStack(), 10));
+
+        Bukkit.addRecipe(new FurnaceRecipe(new NamespacedKey(TorusPlugin.getInstance(), "iron_dust_to_ingot"), new ItemStack(Material.IRON_INGOT), new RecipeChoice.ExactChoice(TorusItem.getById("torus:iron_dust").toItemStack()), 0f, 60));
+        Bukkit.addRecipe(new FurnaceRecipe(new NamespacedKey(TorusPlugin.getInstance(), "copper_dust_to_ingot"), new ItemStack(Material.COPPER_INGOT), new RecipeChoice.ExactChoice(TorusItem.getById("torus:copper_dust").toItemStack()), 0f, 60));
+        Bukkit.addRecipe(new FurnaceRecipe(new NamespacedKey(TorusPlugin.getInstance(), "gold_dust_to_ingot"), new ItemStack(Material.GOLD_INGOT), new RecipeChoice.ExactChoice(TorusItem.getById("torus:gold_dust").toItemStack()), 0f, 60));
     }
 
 }
