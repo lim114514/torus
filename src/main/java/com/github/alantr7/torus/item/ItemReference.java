dc5ca342341bf462d9386a53da73fc72990fd5a3
alantr7 <alan.trumic066@gmail.com>
2025-10-10 14:13:16 +0200

    added ItemReference#compare

--- src/main/java/com/github/alantr7/torus/item/ItemReference.java
+++ src/main/java/com/github/alantr7/torus/item/ItemReference.java
@@ -4,6 +4,8 @@ import com.github.alantr7.torus.TorusPlugin;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Objects;
+
 public class ItemReference {
 
     public final String providerId;
@@ -20,9 +22,29 @@ public class ItemReference {
         return TorusPlugin.getInstance().getItemManager().getItemStackByReference(this);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ItemReference that = (ItemReference) o;
+        return Objects.equals(providerId, that.providerId) && Objects.equals(itemId, that.itemId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(providerId, itemId);
+    }
+
     public static ItemReference parse(String item) {
         int pos = item.indexOf(":");
         return pos == -1 ? new ItemReference("minecraft", item) : new ItemReference(item.substring(0, pos), item.substring(pos + 1));
+    }
+
+    public static ItemReference create(ItemStack stack) {
+        return TorusPlugin.getInstance().getItemManager().createItemReference(stack);
+    }
+
+    public static boolean compare(ItemStack stack1, ItemStack stack2) {
+        return create(stack1).equals(create(stack2));
     }
 
 }
