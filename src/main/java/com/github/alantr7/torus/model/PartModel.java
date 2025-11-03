43bec933da2ce69ab525746ef9bba9b159f6097c
alantr7 <alan.trumic066@gmail.com>
2025-11-03 23:24:09 +0100

    replaced individual and heavier teleportation computation with just teleporting model parent

--- src/main/java/com/github/alantr7/torus/model/PartModel.java
+++ src/main/java/com/github/alantr7/torus/model/PartModel.java
@@ -1,9 +1,9 @@
 package com.github.alantr7.torus.model;
 
-import org.bukkit.Bukkit;
+import io.papermc.paper.entity.TeleportFlag;
 import org.bukkit.Location;
-import org.bukkit.entity.Entity;
-import org.joml.Vector3f;
+import org.bukkit.entity.Display;
+import org.bukkit.event.player.PlayerTeleportEvent;
 
 import java.util.List;
 
@@ -11,51 +11,18 @@ public class PartModel {
 
     public final PartModelTemplate template;
 
-    public final Entity parent;
+    public final Display parent;
 
     public final List<EntityReference> entityReferences;
 
-    public PartModel(PartModelTemplate template, Entity parent, List<EntityReference> entityReferences) {
+    public PartModel(PartModelTemplate template, Display parent, List<EntityReference> entityReferences) {
         this.template = template;
         this.parent = parent;
         this.entityReferences = entityReferences;
     }
 
     public void teleport(Location location) {
-        Vector3f min = new Vector3f(3_000_000, 3_000_000, 3_000_000);
-        Vector3f max = new Vector3f(-3_000_000, -3_000_000, -3_000_000);
-
-        for (EntityReference ref : entityReferences) {
-            if (ref.getEntity() == null)
-                continue;
-
-            Location loc = ref.entity.getLocation();
-            min.x = (float) Math.min(min.x, loc.getX());
-            min.y = (float) Math.min(min.y, loc.getY());
-            min.z = (float) Math.min(min.z, loc.getZ());
-
-            max.x = (float) Math.max(max.x, loc.getX());
-            max.y = (float) Math.max(max.y, loc.getY());
-            max.z = (float) Math.max(max.z, loc.getZ());
-        }
-
-        Vector3f center = new Vector3f(min.x + (max.x - min.x) / 2f, min.y + (max.y - min.y) / 2f, min.z + (max.z - min.z) / 2f);
-
-        // Teleport based on relative coordinates
-        for (EntityReference ref : entityReferences) {
-            if (ref.getEntity() == null)
-                continue;
-
-            Location loc = ref.entity.getLocation();
-            double offsetX = (loc.getX() - center.x);
-            double offsetY = (loc.getY() - center.y);
-            double offsetZ = (loc.getZ() - center.z);
-
-            Location newLocation = location.clone().add(offsetX, offsetY, offsetZ);
-            newLocation.setYaw(loc.getYaw());
-            newLocation.setPitch(loc.getPitch());
-            ref.entity.teleport(newLocation);
-        }
+        parent.teleport(location, PlayerTeleportEvent.TeleportCause.PLUGIN, TeleportFlag.EntityState.RETAIN_PASSENGERS);
     }
 
     public void remove() {
