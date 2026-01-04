b186b22455cd5aa27e6588576028319fdee5bdd8
alantr7 <alan.trumic066@gmail.com>
2026-01-04 18:48:27 +0100

    refactor: Updated plugin version to 0.5.3

--- src/main/java/com/github/alantr7/torus/TorusPlugin.java
+++ src/main/java/com/github/alantr7/torus/TorusPlugin.java
@@ -20,7 +20,7 @@ import com.github.alantr7.torus.world.TorusWorldManager;
 import lombok.Getter;
 import org.bukkit.Bukkit;
 
-@JavaPlugin(name = "Torus", version = "0.5.2", apiVersion = "1.21")
+@JavaPlugin(name = "Torus", version = "0.5.3", apiVersion = "1.21")
 @Relocations(@Relocate(from = "com.github.alantr7.bukkitplugin", to = "com.github.alantr7.torus.bpf"))
 @SoftDepends({"ProtocolLib", "ModelEngine"})
 public class TorusPlugin extends BukkitPlugin {
