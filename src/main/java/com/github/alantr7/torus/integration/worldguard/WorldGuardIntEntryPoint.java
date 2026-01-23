37cb583bb83a05948ea220caaca5ab1e6dfc10d6
alantr7 <alan.trumic066@gmail.com>
2026-01-23 20:59:58 +0100

    chore: Added WorldGuard dependency

---
+++ src/main/java/com/github/alantr7/torus/integration/worldguard/WorldGuardIntEntryPoint.java
@@ -0,0 +1,12 @@
+package com.github.alantr7.torus.integration.worldguard;
+
+import com.github.alantr7.bukkitplugin.annotations.core.RequiresPlugin;
+import com.github.alantr7.bukkitplugin.annotations.core.Singleton;
+
+@Singleton
+@RequiresPlugin("WorldGuard")
+public class WorldGuardIntEntryPoint {
+
+
+
+}
